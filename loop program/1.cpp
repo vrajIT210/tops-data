bde1cff883a7ff38971776cfe6e4025c69333ac8
vrajIT210 <vrajpatel1064@gmail.com>
2024-08-07 13:48:52 +0530

    Delete loop program directory

--- loop program/1.cpp
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-main()
-{
-	for(int i=972;i>=897;i--)
-	{
-		printf("%d\n",i);
-	}
-}
--- loop program/5.cpp
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-main(){
-	int fact=1;
-	for(int i=1;i<=5;i++)
-	{
-		fact*=i;
-	}
-	printf("%d",fact);
-}
