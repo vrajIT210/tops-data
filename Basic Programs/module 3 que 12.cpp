dee69c220ecc4766f780f1c78ea4f04324436eeb
vrajIT210 <vrajpatel1064@gmail.com>
2024-08-07 13:48:02 +0530

    Delete Basic Programs directory

--- Basic Programs/module 3 que 12.cpp
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-main()
-{
-	int a;
-	printf("enter number of student : ");
-	scanf("%d",&a);
-	printf("\nMultiplication : %d",a*5);
-}
--- Basic Programs/module 3 que 18.cpp
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-main()
-{
-	int a;
-	printf("enter person's 1 month salary : ",a);
-	scanf("%d",&a);
-	printf("\nAnnual salary :%d", a*12);
-}
